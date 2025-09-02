a27be02f10e1379961cb67c1ba4f8ac0e10a1685
kedhar123 <bkedhar@gitam.in>
2025-09-02 23:47:04 +0530

    homepage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { HomeBackground } from './components/HomeBackground'
+import { NavbarDemo } from './assets/navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <NavbarDemo />
+        <HomeBackground />
+      </div>
+    </>
+  )
+}
+
+export default App
